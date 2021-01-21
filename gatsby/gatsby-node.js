29ffbe0fe535a06e71b7d5efa1ac1d09d73cf7e5
Lara Krefski <lkrefski@workwave.com>
2021-01-21 10:30:10 -0500

    change

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -120,7 +120,6 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
   // 4. Loop from 1 to n and create the pages for them
   Array.from({ length: pageCount }).forEach((_, i) => {
-    console.log(`Creating page ${i}`);
     actions.createPage({
       path: `/slicemasters/${i + 1}`,
       component: path.resolve('./src/pages/slicemasters.js'),
